b0549cb9c10598cb171b23c7483163046247abb7
김태연 <kam_ta_yon@naver.com>
2024-01-23 15:24:33 +0900

    2024-01-23

--- practice.py
+++ practice.py
@@ -1,27 +1,7 @@
+my_set = {'a', 'b', 'c', 1, 2, 3}
 
-import requests
-from pprint import pprint as print
+my_set.add(4)
+print(my_set)
 
-API_URL = 'https://jsonplaceholder.typicode.com/users'
-# API 요청
-response = requests.get(API_URL)
-# JSON -> dict 데이터 변환
-parsed_data = response.json()
-
-# 응답 데이터 출력
-# print(response)
-
-# 변환 데이터 출력
-# print(parsed_data[0])
-
-# dummy_data = []
-# for i in range(10) :
-#     temp_dict = {'company': parsed_data['company']['name'],
-#                  'lat': parsed_data[]
-#                  }
-#     dummy_data.append(temp_dict)
-    
-    
-# print(dummy_data)
-    
-print(parsed_data[0]['company'])
+my_set.clear()
+print(my_set)
