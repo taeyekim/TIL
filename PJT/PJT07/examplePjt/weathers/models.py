9392447e69a8b560238d7513ed41e7fa1df48a8a
taeyekim <kam_ta_yon@naver.com>
2024-04-26 17:58:45 +0900

    2024-04-26

---
+++ PJT/PJT07/examplePjt/weathers/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Weather(models.Model):
+    dt_txt = models.DateTimeField()  # 관측 시간
+    temp = models.FloatField()       # 온도
+    feels_like = models.FloatField() # 체감 온도(기본값: 켈빈온도)
